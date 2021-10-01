84f4ec265bc9b0f7d2d2130ae4e23bb3e45c7b73
Gabriel Tinoco <gabrielslai@gmail.com>
2021-10-01 12:29:58 -0300

    Added form validation logic and comment header.

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -1,11 +1,22 @@
-// IIFE -- Immediately Invoked Function Expression
+/*
+    File name: app.js
+    Student: Gabriel Dias Tinoco
+    StudentID: 301160373
+    Date: 10/01/21
+*/
+
 (function(){
-
-    function Start()
-    {
-        console.log("App Started...");
+    //Gets the submit button object, and assigns it the formSubmission funciton to the button.
+    let submitButton = $("input")[4];
+    submitButton.addEventListener("click", formSubmission);
+    
+    //Checks the form's validation status, and if valid redirects the user back to the home page.
+    function formSubmission() {
+        let form = $("form")[0];
+        if (form.checkValidity() === true) {
+            console.log("Success.")
+            window.location.href = "/home";
+        }
+        else { form.reportValidity(); }
     }
-
-    window.addEventListener("load", Start);
-
 })();
